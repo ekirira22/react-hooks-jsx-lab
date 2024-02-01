d568e253176455f01bd1fd760ddfd63a120eb89e
Eric Maranga <ekirira22@gmail.com>
2024-02-01 15:58:54 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui quos hic ratione vitae at praesentium sunt quas laboriosam, commodi est repellat quae, eaque iusto eveniet repellendus ea dicta tempore exercitationem?</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  )
 }
 
 export default About;
